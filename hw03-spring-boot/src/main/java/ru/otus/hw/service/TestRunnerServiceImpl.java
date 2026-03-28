567796743d4fe85f9b86c85ea1e2e65212dcf407
Natali Korban <natafuuuck@gmail.com>
2026-03-28 18:08:53 +0300

    правки по комментариям

--- hw03-spring-boot/src/main/java/ru/otus/hw/service/TestRunnerServiceImpl.java
+++ hw03-spring-boot/src/main/java/ru/otus/hw/service/TestRunnerServiceImpl.java
@@ -1,13 +1,12 @@
 package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
+import ru.otus.hw.exceptions.QuestionReadException;
 
 @Service
 @RequiredArgsConstructor
-public class TestRunnerServiceImpl implements TestRunnerService, CommandLineRunner {
+public class TestRunnerServiceImpl implements TestRunnerService {
 
     private final TestService testService;
 
@@ -15,16 +14,18 @@ public class TestRunnerServiceImpl implements TestRunnerService, CommandLineRunn
 
     private final ResultService resultService;
 
+    private final IOService ioService;
+
     @Override
     public void run() {
-        var student = studentService.determineCurrentStudent();
-        var testResult = testService.executeTestFor(student);
-        resultService.showResult(testResult);
+        try {
+            var student = studentService.determineCurrentStudent();
+            var testResult = testService.executeTestFor(student);
+            resultService.showResult(testResult);
+        } catch (QuestionReadException e) {
+            ioService.printLine("Error: Failed to load test data. Please try again later.");
+        } catch (Exception e) {
+            ioService.printLine("Error: An unexpected error occurred while running the test. Please try again later.");
+        }
     }
-
-    @Override
-    public void run(String... args) throws Exception {
-        run();
-    }
-
 }
